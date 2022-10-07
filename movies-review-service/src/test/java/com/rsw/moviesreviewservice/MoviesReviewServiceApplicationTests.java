1a7d7b690f6d40855cf63050cc21347fa81bdae6
Adrian <adrian.talonia11@gmail.com>
2022-10-07 11:57:42 -0500

    add new project movie-review-service

---
+++ movies-review-service/src/test/java/com/rsw/moviesreviewservice/MoviesReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsw.moviesreviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesReviewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
